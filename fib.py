ce1833a545e5a61a3744601dc4c1c2fe60b557b0
Caileb C <cookcaileb99@gmail.com>
2026-01-11 10:32:41 +0000

    Slight optimization by chaning if/else logic. stripped time to be easier to read

--- fib.py
+++ fib.py
@@ -4,14 +4,12 @@ sys.setrecursionlimit(200001)
 
 i = [0, 1]
 def fib(i):
-    if len(i) < 200000:
-        return i
-    elif i[-1] + i[-2] > 0:
+    if len(i) < 200000 and i[-1] + i[-2] > 0:
         i.append(i[-1] + i[-2])
         return fib(i)
     else:
         return i
-print(f"Starting Fibonacci sequence generation at {datetime.datetime.now()}")
+print(f"Starting Fibonacci sequence generation at {datetime.datetime.now().strftime('%H:%M:%S.%f')}")
 fib(i)
-print(f"Fibonacci sequence generation completed at {datetime.datetime.now()}")
+print(f"Fibonacci sequence generation completed at {datetime.datetime.now().strftime('%H:%M:%S.%f')}")
 print(f"Generated {len(i)} Fibonacci numbers.")
